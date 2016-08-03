6ff098b55f3e814337b5fd6332fb449ac9646288
Bent Tranberg <bent.tranberg@gmail.com>
2016-08-03 19:42:12 +0200

    Printing from branch Y.

--- Demo/Program.fs
+++ Demo/Program.fs
@@ -3,10 +3,13 @@ let branchA () = printfn "This is branch A."
 
 let branchB () = printfn "This is branch B."
 
+let branchY () = printfn "This is branch Y."
+
 [<EntryPoint>]
 let main argv =
     printfn "%A" argv
     branchA ()
     branchB ()
+    branchY ()
     System.Console.ReadKey () |> ignore
     0 // return an integer exit code
