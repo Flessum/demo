029f7011f8e1e29b538e3141235a9521efa7b54e
BentTranberg <bent.tranberg@gmail.com>
2016-08-03 18:55:54 +0200

    Reintegrated branchA to trunk.

--- Demo/Program.fs
+++ Demo/Program.fs
@@ -1,4 +1,9 @@
-﻿[<EntryPoint>]
+﻿
+let branchA () = printfn "This is branch A."
+
+[<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    branchA ()
+    System.Console.ReadKey () |> ignore
     0 // return an integer exit code
